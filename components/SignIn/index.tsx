61bc80bacfbcb7388802702c3d1c51edad4694dc
Juan-JoseJos418 <14027448-Juan-JoseJos418@users.noreply.replit.com>
2025-02-08 22:52:41 +0000

    User checkpoint: Update donation button styling: removed button, increased font size, enlarged icon.

--- components/SignIn/index.tsx
+++ components/SignIn/index.tsx
@@ -52,7 +52,7 @@ export const SignIn = () => {
               }}
               className="px-4 py-2 bg-purple-400/80 text-white rounded-xl hover:bg-purple-500 transition-colors duration-200 mr-2"
             >
-              Donate WLD
+              Donate
             </button>
             <button
               onClick={() => signOut()}
