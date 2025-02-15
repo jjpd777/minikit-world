773c8a3f3bd4da427d026e774505e3a9cc32f1bf
jjpd777 <juanjo.palacioduque@gmail.com>
2025-02-15 09:22:41 +0000

    Assistant checkpoint: Added useEffect import
    Assistant generated file changes:
    - components/SignIn/index.tsx: Add useEffect import
    
    ---
    
    User prompt:
    
    useEffect is not defined

--- components/SignIn/index.tsx
+++ components/SignIn/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { MiniKit, VerificationLevel } from "@worldcoin/minikit-js";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 
